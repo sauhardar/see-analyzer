93f7d468e316538be652eb117f7ea520b8741090
Sauharda Rajbhandari <sauharda19@gmail.com>
2022-04-13 17:59:19 -0400

    Try analysis; nothing significant

--- scraper.py
+++ scraper.py
@@ -71,13 +71,19 @@ def main_scraper():
         os.makedirs("results")
 
     driver = init_scraper()
-    initial_symbol = 300_467
+    initial_symbol = 302_467
     to_add = 0
     symbol = initial_symbol
     start = timeit.default_timer()
     errors = 0
-    file_storage_increment = 500
-    for _ in range(2000):
+    file_storage_increment = 1000
+    """
+    00304119W,3.95,COMPLETED
+    00402467M,2.55,COMPLETED
+
+    The current method started at 302_467 and then restarted at 402_467, so missed otu on 400_001 until 402_466
+    """
+    for _ in range(10_000):
         try:
             driver = scrape_iframe(driver, padder(symbol))
             symbol = symbol + 1
@@ -87,7 +93,7 @@ def main_scraper():
             if len(symbols) % file_storage_increment == 0:
                 df = create_df()
                 # os.remove(f"results/first_{len(symbols) - file_storage_increment}.csv") if len(symbols) > file_storage_increment else None
-                df.to_csv(f"results/first_{len(symbols)}.csv", index=False)
+                df.to_csv(f"results/{len(symbols)}.csv", index=False)
         except Exception as e:
             print(e)
 
